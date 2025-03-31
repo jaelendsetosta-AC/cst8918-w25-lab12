5991c9f1588a35a3054fc7dbf9276671d54236ca
jaelendsetosta-AC <seto0046@algonquinlive.com>
2025-03-31 17:52:28 -0400

    update address prefix

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_subnet" "subnet" {
   name = "seto-a12-subnet"
   resource_group_name = azurerm_resource_group.a12.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix = "10.0.1.0/24"
+  address_prefix = ["10.0.1.0/24"]
 }
 
 output "resource_group_name" {
