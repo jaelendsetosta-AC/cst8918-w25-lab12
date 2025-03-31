f74bda9564cf26b65bd90083699de41ae52a8bdf
jaelendsetosta-AC <seto0046@algonquinlive.com>
2025-03-31 17:56:10 -0400

    auto-format terraform files

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -4,26 +4,26 @@ resource "azurerm_resource_group" "a12" {
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name = "seto-a12-vnet"
-  location = "canadacentral"
+  name                = "seto-a12-vnet"
+  location            = "canadacentral"
   resource_group_name = azurerm_resource_group.a12.name
-  address_space = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet" {
-  name = "seto-a12-subnet"
-  resource_group_name = azurerm_resource_group.a12.name
+  name                 = "seto-a12-subnet"
+  resource_group_name  = azurerm_resource_group.a12.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 output "resource_group_name" {
-  value = azurerm_resource_group.a12.name
+  value       = azurerm_resource_group.a12.name
   description = "The name of the resource group"
 }
 
 output "resource_group_location" {
-  value = azurerm_resource_group.a12.location
+  value       = azurerm_resource_group.a12.location
   description = "The location of the resource group"
 }
 
