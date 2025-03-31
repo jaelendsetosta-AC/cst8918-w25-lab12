14c87ab428ef90b8cebf8982e5e1568110a4cec9
BenYee15 <yee00034@algonquinlive.com>
2025-03-31 15:50:17 -0400

    part 5 of lab

--- infra/tf-app/terraform.tf
+++ infra/tf-app/terraform.tf
@@ -11,9 +11,11 @@ terraform {
     storage_account_name = "setogithubactions"
     container_name       = "tfstate"
     key                  = "prod.app.tfstate"
+    use_oidc             = true
   }
 }
 
 provider "azurerm" {
   features {}
+  use_oidc = true
 }
