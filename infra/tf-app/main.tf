c8415079dce3ece21437206f4b6b59ca77067db3
jaelendsetosta-AC <seto0046@algonquinlive.com>
2025-03-31 18:11:51 -0400

    moved outputs to outputs.tf and created empty variables files

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -16,14 +16,3 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
-
-output "resource_group_name" {
-  value       = azurerm_resource_group.a12.name
-  description = "The name of the resource group"
-}
-
-output "resource_group_location" {
-  value       = azurerm_resource_group.a12.location
-  description = "The location of the resource group"
-}
-
---
+++ infra/tf-app/outputs.tf
@@ -0,0 +1,10 @@
+output "resource_group_name" {
+  value       = azurerm_resource_group.a12.name
+  description = "The name of the resource group"
+}
+
+output "resource_group_location" {
+  value       = azurerm_resource_group.a12.location
+  description = "The location of the resource group"
+}
+
