c744d7757ed563b9bfd3f88d432db8855d43f3b3
jaelendsetosta-AC <seto0046@algonquinlive.com>
2025-03-31 17:50:51 -0400

    changed declaration of variable and update infra-ci-cd

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -3,6 +3,20 @@ resource "azurerm_resource_group" "a12" {
   location = "canadacentral"
 }
 
+resource "azurerm_virtual_network" "vnet" {
+  name = "seto-a12-vnet"
+  location = "canadacentral"
+  resource_group_name = azurerm_resource_group.a12.name
+  address_space = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name = "seto-a12-subnet"
+  resource_group_name = azurerm_resource_group.a12.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix = "10.0.1.0/24"
+}
+
 output "resource_group_name" {
   value = azurerm_resource_group.a12.name
   description = "The name of the resource group"
@@ -13,16 +27,3 @@ output "resource_group_location" {
   description = "The location of the resource group"
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name = "seto-a12-vnet"
-  location = "canadacentral"
-  resource_group_name = azurerm_resource_group.rg.name
-  address_space = ["10.0.0.0/16"]
-}
-
-resource "azurerm_subnet" "subnet" {
-  name = "seto-a12-subnet"
-  resource_group_name = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix = "10.0.1.0/24"
-}
